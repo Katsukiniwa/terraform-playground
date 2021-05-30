b166ea177bf7f7ac611af33bdbed0b38aac3cbe3
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-05-30 21:32:04 +0900

    update

--- section7/7-14.tf
+++ section7/7-14.tf
@@ -4,3 +4,11 @@ resource "aws_subnet" "private_0" {
   availability_zone       = "ap-northeast-1a"
   map_public_ip_on_launch = false
 }
+
+resource "aws_subnet" "private_1" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.20.66.0/24"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = false
+}
+
